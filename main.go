fada79227284369405ee505a296bbfa2d0ec0d74
anandhmaps <anandh.maps@gmail.com>
2024-08-16 07:44:27 +0800

    Further updates

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	handler := handlers.NewHandler()
 
 	r.POST("/api/posts", handler.CreatePost)
-	r.GET("/api/posts/:id", handler.GetPost)
+	r.GET("/api/posts/:id", handler.GetPostById)
 	r.POST("/api/comments", handler.CommentOnPost)
 	r.DELETE("/api/comments/:id", handler.DeleteComment)
 	r.GET("/api/timeline", handler.ViewTimeline)
